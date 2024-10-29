e1d0a5f210451414b305b81bd876e32d1c197233
Klaska <laska.rayl@gmail.com>
2024-10-30 00:44:01 +0100

    add style and grid function

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-const container = document.querySelector('#container');
+const game = document.querySelector('#game');
+let gridNum = 16;
+
+function createGrid() {
+    for (let i = 0; i < gridNum; i++) {
+        let row = document.createElement('div');
+        row.classList.add('row');
+
+            for (let j = 1; j <= gridNum; j++) {
+                let cell = document.createElement('div');
+                cell.classList.add('cell');
+                row.appendChild(cell);
+            };
+        game.appendChild(row);
+    };
+};
+createGrid();
 
