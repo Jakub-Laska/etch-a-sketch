5006e7abce51e2f467b343026120b477675e7143
Klaska <laska.rayl@gmail.com>
2024-10-30 19:50:19 +0100

    add hover effect and clear function

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 const game = document.querySelector('#game');
-let gridNum = 10;
+const rightKnob = document.querySelector('#rightKnob');
+let gridNum = 2;
+let selectedColor = 'red';
+let backgroundColor = '#a7a7a7';
+const cells = [];
 
-function createGrid() {
-    for (let i = 0; i < gridNum; i++) {
-        let row = document.createElement('div');
-        row.classList.add('row');
+// grid creation as well as adding every cell into an array
+    function createGrid() {
+        for (let i = 0; i < gridNum; i++) {
+            let row = document.createElement('div');
+            row.classList.add('row');
 
-            for (let j = 1; j <= gridNum; j++) {
-                let cell = document.createElement('div');
-                cell.classList.add('cell');
-                row.appendChild(cell);
-            };
-        game.appendChild(row);
+                for (let j = 1; j <= gridNum; j++) {
+                    let cell = document.createElement('div');
+                    cell.classList.add('cell');
+                    cells.push(cell);
+                    row.appendChild(cell);
+                };
+            game.appendChild(row);
+        };
     };
-};
 createGrid();
 
+// hover effect
+    cells.forEach(element => {
+        element.addEventListener('mouseenter', () => {
+            element.style.backgroundColor = selectedColor;
+        });
+    });  
+
+
+// clear
+rightKnob.addEventListener('click', () => {
+    cells.forEach(element => {
+        element.style.backgroundColor = backgroundColor;
+    })
+});
